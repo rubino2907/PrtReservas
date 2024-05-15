794fd22a0fd32855030c4945fe133e6b28545100
Ruben <rubenfilipe2908@gmail.com>
2024-05-15 16:07:40 +0100

    DesignEdits

--- src/app/services/extraServices/icon.service.ts
+++ src/app/services/extraServices/icon.service.ts
@@ -12,7 +12,7 @@ export class IconService {
     // Aqui você pode retornar a lista de ícones disponíveis
     // Por exemplo, você pode listar os nomes dos arquivos de ícone no seu diretório de ícones
     // Substitua este exemplo pelo seu método real de obter a lista de ícones
-    const iconList: string[] = ['CarroAzul.png', 'carro_vermelho.png', 'carro_verde.png','carrinhaAzul.png', 'CarrinhaVermelha.png', 'camiaoVerde.png'];
+    const iconList: string[] = ['carroAzul.png', 'carro_vermelho.png','carrinhaAzul.png', 'CarrinhaVermelha.png', 'camiaoVerde.png'];
     return of(iconList);
   }
 }
